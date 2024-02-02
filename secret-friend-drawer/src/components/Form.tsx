dd78512f24c5f4c03d7723df5d7b41f734bf9150
Carlos Eduardo <dev.carloseddie@outlook.com>
2024-02-02 11:16:30 -0300

    filled and duplicated name and error message tests

--- secret-friend-drawer/src/components/Form.tsx
+++ secret-friend-drawer/src/components/Form.tsx
@@ -1,8 +1,29 @@
+import { useRef, useState } from "react"
+import { useAddParticipant } from "../state/hook/useAddParticipant"
+import { useErrorMessage } from "../state/hook/useErrorMessage"
+
 const Form = () => {
+
+    const [name, setName] = useState('')
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const addToList = useAddParticipant()
+
+    const errorMessage = useErrorMessage()
+
+    const addParticipant = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        addToList(name)
+        setName('')
+        inputRef.current?.focus()
+    }
+
     return (
-        <form>
-            <input type="text" placeholder="enter the names of the participants" />
-            <button disabled={true}>Add</button>
+        <form onSubmit={addParticipant}>
+            <input ref={inputRef} value={name} onChange={event => setName(event.target.value)} type="text" placeholder="enter the names of the participants" />
+            <button disabled={!name}>Add</button>
+            {errorMessage && <p role="alert">{errorMessage}</p>}
         </form>
     )
 }
